d256c019a2c15ba7aa9bb7b342b193de1d6af6ac
Pachi-Dev <pachi.claros@gmail.com>
2023-09-11 16:37:54 -0600

    update cols

--- src/App.jsx
+++ src/App.jsx
@@ -73,13 +73,13 @@ function App () {
         </Container>
         <Container fluid className='pt-5 mt-5'>
           <Row className='mt-5'>
-            <Col className='my-auto'>
+            <Col md={3} className='my-auto'>
               <img src='/djinvitado.png' alt='ITM MUSIC FEST' className='w-100' />
             </Col>
-            <Col style={{ marginTop: '-50px' }}>
+            <Col md={6} style={{ marginTop: '-50px' }}>
               <img src='/blackeyedpeas2.png' alt='ITM MUSIC FEST' className='w-100' />
             </Col>
-            <Col className='my-auto'>
+            <Col md={3} className='my-auto'>
               <img src='/djinvitado2.png' alt='ITM MUSIC FEST' className='w-100' />
             </Col>
           </Row>
